ae3f4bfbbe0d2378ca783d61a9792e370ee8181e
Ezabulbary <ezabul.bary@gmail.com>
2022-03-16 18:02:20 +0600

    case convert

---
+++ case-convert.js
@@ -0,0 +1,11 @@
+const anthem = 'amar sonar bangla ami tomay valobasi';
+const search = 'valo';
+
+const userInput = 'BLACKpinK';
+const savedUserName = 'blackpink';
+if(userInput.toLowerCase == savedUserName.toLowerCase){
+    console.log('user exists')
+}
+/* if(userInput.toUpperCase == savedUserName.toUpperCase){
+    console.log('user exists')
+} */
