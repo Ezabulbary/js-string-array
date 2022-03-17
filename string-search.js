54e993fc593f2b937b40dcf979a1bd18ab20f72e
Ezabulbary <ezabul.bary@gmail.com>
2022-03-17 11:58:52 +0600

    string searching

---
+++ string-search.js
@@ -0,0 +1,22 @@
+const products = [
+    'Dell insparon 15 3000',
+    'hp Model 4x59 silver color',
+    'modulas to thin Laptop',
+    'htc lower price Phone',
+    'Dell new model',
+    'Sony TV old model',
+    'LG butterfly',
+];
+
+const searching = 'Dell';
+
+// indexOf
+
+const output = [];
+for(const product of products){
+    if(product.toLowerCase().indexOf(searching.toLowerCase()) != -1){
+        output.push(product);
+    }
+}
+
+console.log(output);
