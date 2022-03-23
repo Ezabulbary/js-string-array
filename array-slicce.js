ac679e51654bb56abc5e64a5e8e24c1e58831cbe
Ezabulbary <ezabul.bary@gmail.com>
2022-03-23 13:37:16 +0600

    array slice

--- array-slicce.js
+++ array-slicce.js
@@ -1,4 +1,6 @@
 const numbers = [15, 27, 33, 43, 52, 64, 79, 86, 90];
+
+// slice 
 const numberSlice = numbers.slice(4, 7);
 console.log(numberSlice);
 console.log(numbers);
@@ -6,4 +8,8 @@ console.log(numbers);
 // splice 
 const numberSplice = numbers.splice(3, 4);
 console.log(numberSplice);
+console.log(numbers);
+
+const numberSplice2 = numbers.splice(2, 3, 35, 41, 58);
+console.log(numberSplice2);
 console.log(numbers);
